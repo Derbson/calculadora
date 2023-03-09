5cac987afaedb6db1253b5a4386e971d9d3f4c2e
Derbson <pedrin.olinda@gmail.com>
2023-03-09 08:37:43 -0300

    adição de tema

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ from PySide6.QtGui import QIcon
 from window import MyWindow
 from display import Display
 from variables import WINDOW_INCO_PATH
+from style import setupTheme
+from info import Info
 
 
 if __name__ == '__main__':
     # cria app e janela
     app = QApplication(sys.argv)
+    setupTheme()
     window = MyWindow()
     
     # define icon
     icon = QIcon(str(WINDOW_INCO_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+    
+    #info
+    info = Info('25 * 5')
+    window.addWidgetToVLayout(info)
     
     # Display
     display = Display()
